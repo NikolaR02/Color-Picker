6a97e3e9272a459dec1acabae4c37311244dc6ef
Nicola <nik.nik.raich@gmail.com>
2022-09-28 09:28:22 +0200

    2. Cambiar el color de fondo

--- src/main/java/com/example/colorpicker/HelloApplication.java
+++ src/main/java/com/example/colorpicker/HelloApplication.java
@@ -1,51 +1,40 @@
 package com.example.colorpicker;
 
 import javafx.application.Application;
-import javafx.event.Event;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        Label l = new Label("no selected color ");
-
-
-
-        stage.setTitle("Color Picker App");
-
+    public void start(Stage stage) {
+        // declaración de los elementos. Un campo de texto y el elemento ColorPicker
+        Label l = new Label("no hay color elegido ");
+        l.setStyle("-fx-background-color: #ccccff;");
         ColorPicker colorPicker = new ColorPicker();
+
+        // los elementos estructorados en HBox y VBox
         HBox hBox = new HBox(colorPicker);
         HBox hBox1 = new HBox(l);
         VBox vBox = new VBox(hBox,hBox1);
-        vBox.setPadding(new Insets(150, 100, 150, 100));
+        vBox.setPadding(new Insets(100, 100, 100, 100));
+        Scene scene = new Scene(vBox, 400, 300);
 
-        Scene scene = new Scene(vBox, 600, 400);
-
-        colorPicker.setOnAction(new EventHandler() {
-            public void handle(Event t) {
-                l.setText("Color elegido: " + colorPicker.getValue());
-                //vBox.setStyle("-fx-background-color: " + colorPicker.getValue() + ";");
-            }
+        // acción al elegir el color
+        colorPicker.setOnAction(t -> {
+            l.setText("Color elegido en hex: " + colorPicker.getValue());
+            vBox.setStyle("-fx-background-color: #" + colorPicker.getValue().toString().substring(2) + ";");
         });
 
-
+        // escena
+        stage.setTitle("Color Picker App");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-
-    }
+    public static void main(String[] args) {launch();}
 }
